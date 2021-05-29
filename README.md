fff6286acf60bf0269e07be724f974fc35585b8e
jarekratajski <jratajski@gmail.com>
2021-05-29 14:25:46 +0200

    częściowy kod

---
+++ README.md
@@ -0,0 +1,43 @@
+1. Zadania:
+
+1.1 zrób listę funkcyjną (główne)
+
+1.2 definiowanie listy, budowanie listy
+
+1.3 wyświetlenie list
+
+1.4 suma elementów
+
+1.5 suma dużej listy (100000 elementów)
+
+1.6 odwracanie listy (reverse)
+
+1.7 powtarzanie  n elementów
+
+1.8 generyczna suma
+
+1.9 max / best element
+
+1.10 konkatenacja
+
+1.11 filtrowanie
+
+1.12 qsort
+
+
+2. Gierka funkcyjna
+
+Najgorsze diablo jakie widzieliście :-) - nie ma nawet potworów
+
+http://setblack.pl:3000
+
+kod servera (kotlin):
+https://github.com/jarekratajski/damagerGame
+klient (typescript/react)
+https://github.com/jarekratajski/damagerFront
+
+
+
+i doklejcie 
+
+https://code-with-me.jetbrains.com/1qLy6nX3qoRYJmFyN9ceQA#p=IU&fp=0C168961B2FD589A9A28839A2E8D0095087277507BA80F3A5FE6C16DC165C653
--- src/main/kotlin/lists/List.kt
+++ src/main/kotlin/lists/List.kt
@@ -1,1 +1,64 @@
 package lists
+
+sealed class KList<out T>
+
+object Nil : KList<Nothing>() {
+    override fun toString() = "[]"
+}
+
+data class Cons<T>(val head: T, val tail: KList<T>) : KList<T>() {
+    override fun toString(): String = "$head : $tail"
+}
+
+
+tailrec fun sum(list: KList<Int>, partial: Int = 0): Int = when (list) {
+    is Nil -> partial
+    is Cons -> sum(list.tail, partial + list.head)
+}
+
+tailrec fun <T> show(list: KList<T>, partial: String = ""): String = when (list) {
+    is Nil -> partial + ": []"
+    is Cons -> show(list.tail, "$partial : ${list.head}")
+}
+
+
+tailrec fun range(first: Int, last: Int, partial: KList<Int> = Nil): KList<Int> =
+    if (first >= last) {
+        partial
+    } else {
+        range(first, last - 1, Cons(last - 1, partial))
+    }
+
+tailrec fun <T> KList<T>.reverse(partial: KList<T> = Nil): KList<T> = when (this) {
+        is Nil -> partial
+        is Cons -> this.tail.reverse(Cons(this.head, partial))
+}
+
+//tailrec fun<T> KList<T>.filter( predicate: partial: KList<Int> = Nil): KList<Int> =
+//    if (first >= last) {
+//        partial
+//    } else {
+//        range(first, last - 1, Cons(last - 1, partial))
+//    }
+
+
+
+fun main() {
+
+    val x1 = Cons(1, Nil)
+    val x2 = Cons(2, x1)
+    val x3 = Cons(5, x2)
+    println(x1)
+    println(x2)
+    println(x3)
+    println(sum(x3))
+    val longList = range(1, 10)
+    println(show(longList))
+//    println(sum(longList))
+    val reversed = longList.reverse()
+    print(show(reversed))
+//    println(longList)
+//    val b = Cons("Lublin", Nil)
+//    println(b)
+
+}
